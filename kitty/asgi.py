cc57285ef0631b3a19aeff2d9105d6ca32b2499f
Ritesh Chavan <riteshshivajichavan@gmail.com>
2024-10-02 00:31:35 +0530

    first commit

---
+++ kitty/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for kitty project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kitty.settings')
+
+application = get_asgi_application()
